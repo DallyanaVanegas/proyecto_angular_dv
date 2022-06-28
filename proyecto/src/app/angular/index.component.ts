1c3bedcdd00250d99d6df84cc0b16b01fe12821d
Dallyana Vanegas <davanegas@uce.edu.ec>
2022-06-27 21:00:07 -0500

    hola mundo angular

---
+++ proyecto/src/app/angular/index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'hola-mundo',
+    templateUrl: './index.component.html'
+})
+
+export class HolaMundo{
+    mensaje : String = 'HOLA MUNDO!'
+}
